7d38e7a3a2d99b083d3c29f2165642c032b1f68f
Gousepeer5426 <gousepeer.skgp.5425@gmail.com>
2024-02-25 07:21:12 +0000

    added

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
  this script contains to the deployment script..
 hello guys ...this ;ine was updated again....
 this line updated by dev brach
+=======
+hello iam updating from state branch..
+its very confuse to work on this branch ..
+>>>>>>> 162b2b3 (added)
